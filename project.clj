dd1bd75abf04e2dfc191d0cd49ca5af0eb0b584d
Greg Haskins <greg@manetu.com>
2024-03-25 15:45:20 -0400

    Add missing info needed for clojars deployment
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,10 @@
 (defproject io.github.manetu/lambda-sdk  "0.0.2-SNAPSHOT"
+  :description "An SDK for developing Lambda functions for the Manetu Platform in ClojureScript"
+  :url "https://github.com/manetu/lambda-sdk-clj"
+  :license {:name "Apache License 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"
+            :year 2023
+            :key "apache-2.0"}
   :min-lein-version "2.9.0"
   :dependencies [[org.clojure/clojure "1.11.2"]
                  [org.clojure/clojurescript "1.11.132"
