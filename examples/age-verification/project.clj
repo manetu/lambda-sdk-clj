a58fabec4f05705035f547d5a1e3164a396938b9
Greg Haskins <greg@manetu.com>
2024-03-25 15:59:08 -0400

    Point examples to offical clojars artifact and add clojars badge
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- examples/age-verification/project.clj
+++ examples/age-verification/project.clj
@@ -4,7 +4,7 @@
                  [org.clojure/clojurescript "1.11.132"
                   :exclusions [com.google.javascript/closure-compiler-unshaded
                                org.clojure/google-closure-library]]
-                 [io.github.manetu/lambda-sdk "0.0.1-SNAPSHOT"]
+                 [io.github.manetu/lambda-sdk "0.0.2"]
                  [thheller/shadow-cljs "2.27.1"]
                  [cljstache "2.0.6"]
                  [com.andrewmcveigh/cljs-time "0.5.2"]]
--- examples/hello-cljs/project.clj
+++ examples/hello-cljs/project.clj
@@ -4,6 +4,6 @@
                  [org.clojure/clojurescript "1.11.132"
                   :exclusions [com.google.javascript/closure-compiler-unshaded
                                org.clojure/google-closure-library]]
-                 [io.github.manetu/lambda-sdk "0.0.1-SNAPSHOT"]
+                 [io.github.manetu/lambda-sdk "0.0.2"]
                  [thheller/shadow-cljs "2.27.1"]]
   :source-paths ["src"])
